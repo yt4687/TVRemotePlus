914bd5c06dd0e7b42b1cd9d0609dd497e45508c5
yt4687 <35659282@users.noreply.github.com>
2020-07-26 15:27:45 +0900

    Update stream.php

--- modules/stream.php
+++ modules/stream.php
@@ -784,11 +784,11 @@
 			
 			// TSTaskを終了する
 			if ($TSTask_shutdown == 'true'){ // 強制終了
-				win_exec('taskkill /F /IM '.tstask_exe);
-				win_exec('taskkill /F /IM '.tstask_SPHD_exe);
+				win_exec('taskkill /F /IM '.$tstask_exe);
+				win_exec('taskkill /F /IM '.$tstask_SPHD_exe);
 			} else { // 通常終了
-				win_exec('taskkill /IM '.tstask_exe);
-				win_exec('taskkill /IM '.tstask_SPHD_exe);
+				win_exec('taskkill /IM '.$tstask_exe);
+				win_exec('taskkill /IM '.$tstask_SPHD_exe);
 			}
 
 			// フォルダ内のTSを削除
