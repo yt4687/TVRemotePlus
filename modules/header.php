60a2ee657f91671a363614cd07e18c5705332770
yt4687 <35659282+yt4687@users.noreply.github.com>
2021-02-10 20:30:42 +0900

    アップデート先のコミット忘れ

--- modules/header.php
+++ modules/header.php
@@ -266,7 +266,7 @@
 <?php
 	if ($update_confirm == 'true'){
 		$update_context = stream_context_create( array('http' => array('timeout' => 5)) );
-		$update = file_get_contents('https://raw.githubusercontent.com/yt4687/TVRemotePlus/master/data/version.txt?_='.time(), false, $update_context);
+		$update = file_get_contents('https://raw.githubusercontent.com/yt4687/TVRemotePlus/SPHD2/data/version.txt?_='.time(), false, $update_context);
 		// 取得したバージョンと現在のバージョンが違う場合のみ
 		if ($update != $version){
 			echo '    <a class="nav-link" href="https://github.com/yt4687/TVRemotePlus/releases" target="_blank" '.
